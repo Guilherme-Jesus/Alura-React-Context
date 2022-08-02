2e7628e804a0c77cce091d56f447c1c008918ad7
Restyled.io <commits@restyled.io>
2022-08-02 18:11:01 +0000

    Restyled by clang-format

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,44 +1,43 @@
-import { Button } from "@material-ui/core";
-import { Container, Titulo, InputContainer } from "./styles";
-import { Input, InputLabel, InputAdornment } from "@material-ui/core";
-import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import { UsuarioContext } from "common/context/Usuario";
+import {Button, Input, InputAdornment, InputLabel} from "@material-ui/core";
+import {UsuarioContext} from "common/context/Usuario";
+import {useContext} from "react";
+import {useNavigate} from "react-router-dom";
+
+import {Container, InputContainer, Titulo} from "./styles";
 
 function Login() {
   const navigate = useNavigate();
-  const { nome, setNome, saldo, setSaldo } = useContext(UsuarioContext);
+  const {nome, setNome, saldo, setSaldo} = useContext(UsuarioContext);
   return (
     <Container>
       <Titulo>Insira o seu nome</Titulo>
       <InputContainer>
         <InputLabel>Nome</InputLabel>
         <Input
-          type="text"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-        />
-      </InputContainer>
-      <InputContainer>
-        <InputLabel>Saldo</InputLabel>
+  type = "text"
+  value = {nome} onChange =
+  { (e) => setNome(e.target.value) } />
+      </InputContainer >
+      <InputContainer><InputLabel>Saldo<
+          /InputLabel>
         <Input
           type="number"
           value={saldo}
           onChange={(e) => setSaldo(e.target.value)}
-          startAdornment={<InputAdornment position="start">R$</InputAdornment>}
-        />
-      </InputContainer>
-      <Button
-        variant="contained"
-        color="primary"
+          startAdornment={<InputAdornment position="start">R$</InputAdornment>
+}
+/>
+      </InputContainer > < Button
+variant = "contained"
+color = "primary"
         onClick={() => {
-          navigate("/feira");
+  navigate("/feira");
         }}
       >
         Avançar
       </Button>
     </Container>
   );
-}
+        }
 
-export default Login;
+        export default Login;
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
-import { UsuarioProvider } from "common/context/Usuario";
+import {UsuarioProvider} from "common/context/Usuario";
 import Carrinho from "pages/Carrinho";
 import Feira from "pages/Feira";
 import Login from "pages/Login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 const Router = () => {
   return (
