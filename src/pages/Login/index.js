0aa435b008816fc4a818bdbb769aa97ce80d6acf
Restyled.io <commits@restyled.io>
2022-08-02 17:39:12 +0000

    Restyled by prettier

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,26 +1,27 @@
-import {Button, Input, InputAdornment, InputLabel} from "@material-ui/core";
+import { Button, Input, InputAdornment, InputLabel } from "@material-ui/core";
 import UsuarioContext from "common/context/Usuario";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-import {Container, InputContainer, Titulo} from "./styles";
+import { Container, InputContainer, Titulo } from "./styles";
 
-function Login({nome, setNome, saldo, setSaldo}) {
+function Login({ nome, setNome, saldo, setSaldo }) {
   const navigate = useNavigate();
   return (
     <Container>
       <UsuarioContext.Consumer>
-        {({nome,setNome,saldo,setSaldo}) => (
+        {({ nome, setNome, saldo, setSaldo }) => (
           <>
             <Titulo>Insira o seu nome</Titulo>
             <InputContainer>
               <InputLabel>Nome</InputLabel>
               <Input
-    type = "text"
-    value = {nome} onChange =
-    { (e) => setNome(e.target.value) } />
-            </InputContainer >
-        <InputContainer><InputLabel>Saldo<
-            /InputLabel>
+                type="text"
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+              />
+            </InputContainer>
+            <InputContainer>
+              <InputLabel>Saldo</InputLabel>
               <Input
                 type="number"
                 value={saldo}
@@ -31,20 +32,19 @@ function Login({nome, setNome, saldo, setSaldo}) {
               />
             </InputContainer>
             <Button
-  variant = "contained"
-  color = "primary"
+              variant="contained"
+              color="primary"
               onClick={() => {
-    navigate("/feira");
+                navigate("/feira");
               }}
             >
               Avançar
             </Button>
           </>
-        )
-}
+        )}
       </UsuarioContext.Consumer>
     </Container>
   );
-      }
+}
 
-      export default Login;
+export default Login;
