76189a809cfe697a3b97fc30f63a7a6b9c81236f
Restyled.io <commits@restyled.io>
2022-08-03 14:21:08 +0000

    Restyled by prettier

--- src/components/Produto/index.js
+++ src/components/Produto/index.js
@@ -1,20 +1,20 @@
-import {IconButton} from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import RemoveIcon from "@material-ui/icons/Remove";
-import {useCarrinhoContext} from "common/context/Carrinho";
-import {memo} from "react";
+import { useCarrinhoContext } from "common/context/Carrinho";
+import { memo } from "react";
 
-import {Container} from "./styles";
+import { Container } from "./styles";
 
-function Produto({nome, foto, id, valor, unidade}) {
-  const {carrinho, adicionarProduto, removerProduto} = useCarrinhoContext();
-  const produtoNoCarrinho =
-      carrinho.find((itemDoCarrinho) => itemDoCarrinho.id === id);
+function Produto({ nome, foto, id, valor, unidade }) {
+  const { carrinho, adicionarProduto, removerProduto } = useCarrinhoContext();
+  const produtoNoCarrinho = carrinho.find(
+    (itemDoCarrinho) => itemDoCarrinho.id === id
+  );
   return (
     <Container>
       <div>
-        <img src={`/assets/${foto}.png`} alt={
-    `foto de ${nome}`} />
+        <img src={`/assets/${foto}.png`} alt={`foto de ${nome}`} />
         <p>
           {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
         </p>
